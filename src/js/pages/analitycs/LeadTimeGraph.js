4ae844c694800e53d4ea6bb85735015f935946dd
Luka Podgorsek <lp5796@student.uni-lj.si>
2018-05-08 09:37:26 +0200

    color fix

--- src/js/pages/analitycs/LeadTimeGraph.js
+++ src/js/pages/analitycs/LeadTimeGraph.js
@@ -21,7 +21,7 @@ import {
 // Custom
 import Loading from '../../components/Loading';
 
-const colorPalette = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffbf', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2'];
+const colorPalette = ['9e0142', 'd53e4f', 'f46d43', 'fdae61', 'fee08b', 'ffffbf', 'e6f598', 'abdda4', '66c2a5', '3288bd', '5e4fa2'];
 const cLen = colorPalette.length;
 
 class LeadTimeGraph extends Component {
@@ -132,7 +132,7 @@ class LeadTimeGraph extends Component {
               <VerticalBarSeries
                 key={Math.random()}
                 data={bar}
-                color={colorPalette[i % cLen]}
+                color={'#' + colorPalette[i % cLen]}
               />
             )
           )}
