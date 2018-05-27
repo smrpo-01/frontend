d2dda04be0a7dacace6f69d67c6bedc7ecd1490a
Luka Podgorsek <lp5796@student.uni-lj.si>
2018-05-27 21:42:45 +0200

    board settings

--- src/js/pages/board/BoardSettings.js
+++ src/js/pages/board/BoardSettings.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+
 
 // Grommet components
 import Article from 'grommet/components/Article';
@@ -15,27 +18,68 @@ import NumberInput from 'grommet/components/NumberInput';
 
 
 class BoardSettings extends Component {
+  constructor() {
+    super();
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      onSubmit: this.onSubmit,
+      error: '',
+    };
+  }
+
+  onSubmit() {
+    this.setState({ onSubmit: null, error: ''});
+    console.log(this.props);
+    this.props.mutateDaysToExpire({
+      variables: { boardId: this.props.boardId, daysToExpire: this.props.daysToExpire  }
+    })
+      .then(() => this.props.closer())
+      .catch((err) => {
+        console.error(err);
+        this.setState({ onSubmit: this.onSubmit, error: err.message.split(':')[1] });
+      });
+  }
+
   render() {
+    // eslint-disable-next-line no-undef
+    const user = sessionStorage.getItem('user');
+    const userRoles = JSON.parse(user).roles;
+    console.log(userRoles);
+
     return (
       <Layer onClose={this.props.closer} closer>
         <Article pad='small'>
-          <Header justify='center'>
+          <Header justify='start'>
             <Heading tag='h3' strong>Uporabniške nastavitve table</Heading>
           </Header>
           <Section pad='none'>
-            <Form style={{ maxWidth: 300 }}>
+            <Form style={{ maxWidth: 310 }}>
+              {(userRoles.indexOf('km') !== -1) &&
+                <FormField label='Število dni do zahtevanega roka za dokončanje'>
+                  <NumberInput
+                    min={0}
+                    value={this.props.daysToExpire}
+                    onChange={e => this.props.updateDaysToExpire(e.target.value)}
+                  />
+                </FormField>
+              }
               <FormField label='Število dni do poteka roka kartice'>
                 <NumberInput
                   min={0}
                   value={this.props.timeframe}
-                  onChange={e => this.props.updateParent(e.target.value)}
+                  onChange={e => this.props.updateTimeframe(e.target.value)}
                 />
               </FormField>
+
             </Form>
           </Section>
+          {(this.state.error !== '') &&
+            <Section className='color-red padding-bottom-0'>{this.state.error}</Section>
+          }
           <Footer pad={{ vertical: 'medium' }} justify='between'>
             <Button secondary label='Prekliči' onClick={this.props.closer} />
-            <Button primary label='Potrdi' onClick={this.props.closer} />
+            <Button primary label='Potrdi' onClick={this.onSubmit} />
           </Footer>
         </Article>
       </Layer>
@@ -46,11 +90,31 @@ class BoardSettings extends Component {
 
 BoardSettings.propTypes = {
   closer: PropTypes.func.isRequired,
-  updateParent: PropTypes.func.isRequired,
+  updateTimeframe: PropTypes.func.isRequired,
+  updateDaysToExpire: PropTypes.func.isRequired,
   timeframe: PropTypes.number.isRequired,
+  daysToExpire: PropTypes.number.isRequired,
+  boardId: PropTypes.number.isRequired,
 };
 
 
 BoardSettings.defaultProps = {};
 
-export default BoardSettings;
+export const mutateDaysToExpire = gql`
+  mutation setBoardExpiration($boardId: Int!, $daysToExpire: Int!) {
+      setBoardExpiration(boardId: $boardId, daysToExpire: $daysToExpire) {
+        board {
+          id
+          daysToExpire
+        }
+      }
+  }
+`;
+
+const BoardSettingsWithMutations = compose(
+  graphql(mutateDaysToExpire, {
+    name: 'mutateDaysToExpire'
+  })
+)(BoardSettings);
+
+export default BoardSettingsWithMutations;
